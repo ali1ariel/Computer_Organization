7959252108a0d06297528a2d8d35792ae5cd69bc
Alisson Ariel S. Machado <aamachado@inf.ufsm.br>
2019-04-02 23:12:20 -0300

    Início
    O trabalho consiste em realizar um simulador de processador em assembly, o código está sendo desenvolvido em shellscript para ser traduzido posteriormente pra assembly.

---
+++ trab1.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+function menu()
+{
+    echo "$1"
+    case "$1" in 
+        l) leitura $2
+        ;;
+        r) simulacao $2
+        ;;
+        d) registradores
+        ;;
+        m) memoria $2 $3
+        ;;
+        *) "opção inválida";echo;menu;;
+    esac
+
+}
+function leitura()
+{
+    if [ -z "$1" ]
+    then
+        echo "faltou o nome do arquivo";
+        return;
+    fi
+    echo "leitura: $1"
+}
+
+function simulacao()
+{
+    if [ -z "$1" ]
+    then
+        echo "faltou o numero de simulações";
+        return;
+    fi
+    echo "simulação: $1"
+}
+
+function registradores()
+{
+    echo "registradores"
+}
+
+function memoria()
+{
+    if [ -z "$1" ]||[ -z "$2" ]
+    then
+        echo "faltou o endereço inicial ou o número de endereços";
+        return;
+    fi
+    echo "memória, endereço inicial: $1, quantidade de endereços $2"
+}
+
+menu $1 $2 $3
